268d2747e38e2b13b43da8e7b8b53e46770ad81f
Алексей <sselicrem@mail.ru>
2017-10-26 18:28:22 +0900

    полноцветный точно сделал

--- bmpwrite.cpp
+++ bmpwrite.cpp
@@ -8,16 +8,15 @@ int N, M, color;
 
 void fillheader(char header[]) {
     int filesize;
-
     // BITMAPFILEHEADER
     BITMAPFILEHEADER bfh;
     ZeroMemory(&bfh, sizeof(bfh));
     
     bfh.bfType = 0x4d42;                        // сигнатура, должно быть 'BM'
-    bfh.bfSize = 54;                            // исправить размер файла
+    bfh.bfSize = color==2 ? 54 + M*ceil(3 * N / 4.0) * 4 : 54 + M*ceil(3 * N / 4.0) * 4 + 256 * 4;                            // исправить размер файла
     bfh.bfReserved1 = 0;                        //
     bfh.bfReserved2 = 0;                        //
-    bfh.bfOffBits = color ? 54 + 256*4 : 54;    // начало пиксельных данных, чб добавляет размер палитры
+    bfh.bfOffBits = color ? 54 : 54 + 256 * 4;    // начало пиксельных данных, чб добавляет размер палитры
     memcpy(header, &bfh, 14);                   // копируем в массив header
 
     // BITMAPINFOHEADER;
@@ -32,24 +31,64 @@ void fillheader(char header[]) {
     memcpy(header + 14, &bih, 40);              // копируем в массив header
 }
 
-void fillpalette(char palette[]) {
-    if (color == 2) return;
-    // если чб, надо заполнять palette байтами
-    // 0 0 0 0 1 1 1 0 2 2 2 0 3 3 3 0 ... 255 255 255 0
-}
 
 void filldata(char data[], int **r, int **g, int **b) {
-    int i, j, linesize;
+    int i, j;
+	int linesize = ceil(3 * N / 4.0) * 4;
+	if (color == 2)
+	{
+		for (i = M - 1; i >= 0; i--)
+		{
+			for (j = 0; j < N; j++)
+			{
+				if (color == 2)
+				{
+					data[(M - 1 - i) * linesize + j * 3] = b[i][j];
+					data[(M - 1 - i) * linesize + j * 3 + 1] = g[i][j];
+					data[(M - 1 - i) * linesize + j * 3 + 2] = r[i][j];
+				}
+			}
+		}
+	}
+	else
+	{
+		for (i=0;i<M;i++)
+		{
+			for (j = N - 1; j >= 0; j--)
+			{
+				data[i * linesize + N - 1 + j*3] = b[i][j];
+				data[i * linesize + N - 1 + j*3 + 1] = b[i][j];
+				data[i * linesize + N - 1 + j *3+ 2] = b[i][j];
+				
+			}
+		}
+	}
     // заполнить данные.
     // учесть: записывать снизу вверх, в цветном файле порядок b, g, r
     // в случае чб есть только b
 }
 
+void fillpalette(char palette[]) {
+	if (color == 2) return;
+	// если чб, надо заполнять palette байтами
+	int i, j=0;
+	int linesize = ceil(3 * N / 4.0) * 4;
+	for (i = 0; i < 1020; i += 4)
+	{
+		palette[i] = j;
+		palette[i + 1] = j;
+		palette[i + 2] = j;
+		palette[i + 3] = 0;
+		j++;
+	}
+	i = 0;
+	// 0 0 0 0 1 1 1 0 2 2 2 0 3 3 3 0 ... 255 255 255 0
+}
 int main(char argc, char* argv[]) {
     int i, j, **r=0, **g=0, **b=0;
     std::ifstream f;
     char *filename; 
-    if (argc > 1) filename = argv[1]; else filename = "input.txt";
+    if (argc > 1) filename = argv[1]; else filename = "inputgray.txt";
     f = std::ifstream(filename);
     if (f.fail()) {
         std::cerr << "could not open file\n";
@@ -57,17 +96,16 @@ int main(char argc, char* argv[]) {
     }
 
     f >> N >> M >> color;
-
     // выделяем память под один (для чб) или три (для цв) массива MxN 
     b = new int*[M]; for (i = 0; i < M; i++) b[i] = new int[N];
-    if (color) {
+    if (color==2) {
         g = new int*[M]; for (i = 0; i < M; i++) g[i] = new int[N];
         r = new int*[M]; for (i = 0; i < M; i++) r[i] = new int[N];
     }
 
     for (i = 0; i < M; i++) {
         for (j = 0; j < N; j++) {
-            if (color)
+            if (color==2)
                 f >> r[i][j] >> g[i][j] >> b[i][j];
             else
                 f >> b[i][j];
@@ -82,7 +120,7 @@ int main(char argc, char* argv[]) {
     char header[54];
     char palette[4 * 256];
 
-    int datasize = color ? M * ceil(3 * N / 4.0) * 4 : 1;   // !!!!!!! размер пиксельных данных, чтобы размер строки был кратен 4 байтам
+    int datasize = color==2 ? M * ceil(3 * N / 4.0) * 4 : M * ceil(3 * N / 4.0) * 4;   // !!!!!!! размер пиксельных данных, чтобы размер строки был кратен 4 байтам
                                                             // исправить для чб случая, сейчас заглушка в виде 1.
     char* data = new char[datasize];
 
@@ -91,7 +129,7 @@ int main(char argc, char* argv[]) {
     filldata(data, r, g, b);    // заполнить массив пиксельных данных
 
     fwrite(header, 1, 54, bmpfile);             // записать заголовки
-    if (!color) {       
+    if (color==0) {       
         fwrite(palette, 1, 4 * 256, bmpfile);   // если чб, записать палитру
     }
     fwrite(data, 1, datasize, bmpfile);         // записать пиксельные данные
